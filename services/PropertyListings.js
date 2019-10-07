c6393f08a1f6300f3eac800aed04495ac71bb64f
stefanagloginic <stefana.gloginic@gmail.com>
2019-10-06 21:00:28 -0700

    [loadAction] putting back changes for param consideration

--- services/PropertyListings.js
+++ services/PropertyListings.js
@@ -6,32 +6,32 @@ import { routes } from '../ATTOM/routes.js';
 export const PropertyListingsService = {
     name: 'listings',
     // at least one of the CRUD methods is required
-    read: function(req, resource, params, configuration, callback) {
+    read: function(req, resource, params = {}, configuration, callback) {
         const { API_STAGE_KEY } = config; // TODO: check env? not working
         const { zipCode } = routes;
 
-        const paramsObj = {
-            postalcode: '90034' // TODO: HARDCODED NEED TO CHANGE TO LOCAL
-        };
+        const { postalCode } = params;
+        const paramsObj = { postalCode };
+
         const baseUrl = obtainPropertyBaseApiUrl();
-        const param = querystring.stringify(paramsObj)
+        const param = querystring.stringify(paramsObj);
 
         const url = `${baseUrl}${zipCode}?${param}`;
 
         superagent
-            .get(url)
-            .set({ apikey: API_STAGE_KEY, Accept: 'application/json' })
-            .then((res) => {
-                let listings;
-                if (res.ok) {
-                    listings = JSON.parse(JSON.stringify(res.text));
-                }
+        .get(url)
+        .set({ apikey: API_STAGE_KEY, Accept: 'application/json' })
+        .then((res) => {
+            let listings;
+            if (res.ok) {
+                listings = JSON.parse(JSON.stringify(res.text));
+            }
 
-                callback(null, listings);
-            }).catch((err) => {
-                if (err) {
-                    console.log('err',err);
-                }
-            });
+            callback(null, listings);
+        }).catch((err) => {
+            if (err) {
+                console.log('err',err);
+            }
+        });
     }
 };
